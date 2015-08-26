1121480bd506f12ef9f657f9a2030246085b5483
Aradhana Duppala <Aradhana_Duppala@intuit.com>
2015-08-25 21:00:38 -0700

    Day 7: Promises

---
+++ app/www/js/app-amd/app_xmlHttpReq.js
@@ -0,0 +1,23 @@
+define(['jquery'], function() {
+
+	var xhr = new XMLHttpRequest();
+
+	xhr.onreadystatechange = function() {
+		console.log(xhr.readyState, xhr.status);
+		if (xhr.readyState === 4 && xhr.status === 200) {
+			console.log(xhr.responseText);
+		}
+	};
+
+	xhr.open("GET", "/api/widgets");
+	xhr.send();
+
+	$.ajax("/api/widgets").success(function(result) {
+		console.dir(result);
+	});
+
+	//request("/api/widgets").then(function() {
+	//	console.dir(arguments);
+	//});
+
+});
