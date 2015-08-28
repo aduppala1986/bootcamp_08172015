0783908126c27bf1382b33eedc2328c8d6923249
Aradhana Duppala <Aradhana_Duppala@intuit.com>
2015-08-28 11:12:40 -0700

    Day 10: Project Structure Cleanup and setup

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,63 +1,119 @@
 module.exports = function(grunt) {
 
-	"use strict";
+  "use strict";
 
-	var
-		path = require("path"),
-		sassFolder = path.join("assets", "sass"),
-		wwwFolder = path.join("app", "www"),
-		cssFolder = path.join(wwwFolder, "css"),
-		jsFolder = path.join(wwwFolder, "js"),
-		libsFolder = path.join(wwwFolder, "libs"),
-		uploadsFolder = path.join("app", "uploads"),
-		indexFile = path.join(__dirname, wwwFolder, "index.html"),//critical for SPA
-		cssMinFiles = {},
-		cssCompressFiles = {},
-		sassFiles = {};
+  var
+    path = require("path"),
+    sassFolder = path.join("assets", "sass"),
+    wwwFolder = path.join("app", "www"),
+    cssFolder = path.join(wwwFolder, "css"),
+    jsFolder = path.join(wwwFolder, "js"),
+    libsFolder = path.join(wwwFolder, "libs"),
+    mediaFolder = path.join(wwwFolder, "libs"),
+    imagesFolder = path.join(wwwFolder, "libs"),
+    uploadsFolder = path.join("app", "uploads"),
+    indexFile = path.join(__dirname, wwwFolder, "index.html"),
+    cssMinFiles = {},
+    cssCompressFiles = {},
+    sassFiles = {};
 
-	sassFiles[path.join(cssFolder, "site.css")] =
-		path.join(sassFolder, "site.scss");
+  sassFiles[path.join(cssFolder, "site.css")] =
+    path.join(sassFolder, "site.scss");
 
-	cssMinFiles[path.join(cssFolder, "site.min.css")] =
-		path.join(cssFolder, "site.css");
+  cssMinFiles[path.join(cssFolder, "site.min.css")] =
+    path.join(cssFolder, "site.css");
 
-	cssCompressFiles[path.join(cssFolder, "site.min.gz.css")] =
-		path.join(cssFolder, "site.min.css");
+  cssCompressFiles[path.join(cssFolder, "site.min.gz.css")] =
+    path.join(cssFolder, "site.min.css");
 
-	grunt.initConfig({
-		httpServer: {
-			port: 8080,
-			rootFolder: wwwFolder,
-			indexFile: indexFile,
-			contentFolders: {
-				cssFolder: cssFolder,
-				jsFolder: jsFolder,
-				libsFolder: libsFolder,
-				uploadsFolder: uploadsFolder
-			}
-		},
+  grunt.initConfig({
+    httpServer: {
+      port: 8080,
+      rootFolder: wwwFolder,
+      indexFile: indexFile,
+      contentFolders: {
+        cssFolder: cssFolder,
+        jsFolder: jsFolder,
+        libsFolder: libsFolder,
+        mediaFolder: mediaFolder,
+        imagesFolder: imagesFolder,
+        uploadsFolder: uploadsFolder
+      }
+    },
     mongoServer: {
       host: "localhost",
       port: 27017,
       dbName: "OnlineFileStorage"
     },
     sass: {
-			main: {
+      main: {
         options: {
           sourcemap: "none"
         },
-				files: sassFiles
-			}
-		},
-		cssmin: {
-			main: {
+        files: sassFiles
+      }
+    },
+    handlebars: {
+      compile: {
+        options: {
+          namespace: "handlebars",
+          amd: true,
+          processName: function(filePath) {
+            return path.basename(filePath, ".min.hbs");
+          },
+          processPartialName: function(filePath) {
+            return path.basename(filePath, ".min.hbs");
+          }
+        },
+        files: {
+          "app/www/js/app/app.templates.hbs.js": ["assets/templates-min/**/*.min.hbs"]
+        }
+      }
+    },
+    jst: {
+      compile: {
+        options: {
+          namespace: "underscore",
+          amd: true
+        },
+        files: {
+          "app/www/js/app/app.templates.tpl.js": ["assets/templates-min/**/*.min.tpl"]
+        }
+      }
+    },
+    htmlmin: {
+      underscore: {
+        options: {
+          removeComments: true,
+          collapseWhitespace: true
+        },
+        expand: true,
+        cwd: 'assets/templates',
+        src: '*.tpl',
+        dest: 'assets/templates-min/',
+        ext: ".min.tpl"
+      },
+      handlebars: {
+        options: {
+          removeComments: true,
+          collapseWhitespace: true
+        },
+        expand: true,
+        cwd: 'assets/templates',
+        src: '*.hbs',
+        dest: 'assets/templates-min/',
+        ext: ".min.hbs"
+      }
+    },
+    cssmin: {
+      main: {
         options: {
           keepSpecialComments: 0,
           sourceMap: false
         },
-				files: cssMinFiles
-			}
-		},
+        files: cssMinFiles
+      }
+    },
     compress: {
       css: {
         options: {
@@ -66,29 +122,47 @@ module.exports = function(grunt) {
         files: cssCompressFiles
       }
     },
-		watch: {
+    watch: {
+      handlebars: {
+        files: ["assets/templates/**/*.hbs"],
+        tasks: ["htmlmin:handlebars", "handlebars"],
+        options: {
+          spawn: false
+        }
+      },
+      underscore: {
+        files: ["assets/templates/**/*.tpl"],
+        tasks: ["htmlmin:underscore", "jst"],
+        options: {
+          spawn: false
+        }
+      },
       css: {
-				files: path.join(sassFolder, "**", "*.scss"),
-				tasks: ["sass","cssmin","compress:css"]
-			}
-		}
-	});
+        files: path.join(sassFolder, "**", "*.scss"),
+        tasks: ["sass", "cssmin", "compress:css"]
+      }
+    }
+  });
 
-	grunt.loadNpmTasks("grunt-contrib-watch");
+  grunt.loadNpmTasks("grunt-contrib-watch");
+  grunt.loadNpmTasks("grunt-contrib-handlebars");
+  grunt.loadNpmTasks("grunt-contrib-jst");
+  grunt.loadNpmTasks("grunt-contrib-htmlmin");
   grunt.loadNpmTasks("grunt-contrib-sass");
   grunt.loadNpmTasks("grunt-contrib-cssmin");
   grunt.loadNpmTasks("grunt-contrib-compress");
 
-	grunt.registerTask("web-server", "start a web server", function() {
+  grunt.registerTask("web-server", "start a web server", function() {
 
-		require("./app/app")({
-			httpServer: grunt.config("httpServer"),
-			mongoServer: grunt.config("mongoServer")
-		});
+    require("./app/app")({
+      httpServer: grunt.config("httpServer"),
+      mongoServer: grunt.config("mongoServer")
+    });
 
-	});
+  });
 
-	grunt.registerTask("default", "start development environment",
-		[ "sass", "cssmin", "compress", "web-server", "watch" ]);
+  grunt.registerTask("default", "start development environment", ["htmlmin", "handlebars", "jst", "sass", "cssmin",
+    "compress", "web-server", "watch"
+  ]);
 
 };
---
+++ gruntarchive/Gruntfile_backbone.js
@@ -0,0 +1,94 @@
+module.exports = function(grunt) {
+
+	"use strict";
+
+	var
+		path = require("path"),
+		sassFolder = path.join("assets", "sass"),
+		wwwFolder = path.join("app", "www"),
+		cssFolder = path.join(wwwFolder, "css"),
+		jsFolder = path.join(wwwFolder, "js"),
+		libsFolder = path.join(wwwFolder, "libs"),
+		uploadsFolder = path.join("app", "uploads"),
+		indexFile = path.join(__dirname, wwwFolder, "index.html"),//critical for SPA
+		cssMinFiles = {},
+		cssCompressFiles = {},
+		sassFiles = {};
+
+	sassFiles[path.join(cssFolder, "site.css")] =
+		path.join(sassFolder, "site.scss");
+
+	cssMinFiles[path.join(cssFolder, "site.min.css")] =
+		path.join(cssFolder, "site.css");
+
+	cssCompressFiles[path.join(cssFolder, "site.min.gz.css")] =
+		path.join(cssFolder, "site.min.css");
+
+	grunt.initConfig({
+		httpServer: {
+			port: 8080,
+			rootFolder: wwwFolder,
+			indexFile: indexFile,
+			contentFolders: {
+				cssFolder: cssFolder,
+				jsFolder: jsFolder,
+				libsFolder: libsFolder,
+				uploadsFolder: uploadsFolder
+			}
+		},
+    mongoServer: {
+      host: "localhost",
+      port: 27017,
+      dbName: "OnlineFileStorage"
+    },
+    sass: {
+			main: {
+        options: {
+          sourcemap: "none"
+        },
+				files: sassFiles
+			}
+		},
+		cssmin: {
+			main: {
+        options: {
+          keepSpecialComments: 0,
+          sourceMap: false
+        },
+				files: cssMinFiles
+			}
+		},
+    compress: {
+      css: {
+        options: {
+          mode: 'gzip'
+        },
+        files: cssCompressFiles
+      }
+    },
+		watch: {
+      css: {
+				files: path.join(sassFolder, "**", "*.scss"),
+				tasks: ["sass","cssmin","compress:css"]
+			}
+		}
+	});
+
+	grunt.loadNpmTasks("grunt-contrib-watch");
+  grunt.loadNpmTasks("grunt-contrib-sass");
+  grunt.loadNpmTasks("grunt-contrib-cssmin");
+  grunt.loadNpmTasks("grunt-contrib-compress");
+
+	grunt.registerTask("web-server", "start a web server", function() {
+
+		require("./app/app")({
+			httpServer: grunt.config("httpServer"),
+			mongoServer: grunt.config("mongoServer")
+		});
+
+	});
+
+	grunt.registerTask("default", "start development environment",
+		[ "sass", "cssmin", "compress", "web-server", "watch" ]);
+
+};
