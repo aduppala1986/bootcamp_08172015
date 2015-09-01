6438bade341a8b6928c3ce9ba29c4f35924eae53
Aradhana Duppala <Aradhana_Duppala@intuit.com>
2015-09-01 09:34:06 -0700

    Day 12: Added Marionette and Dojo code and backed up old app.js code

---
+++ app/www/js/app/app_account_demo.js
@@ -0,0 +1,42 @@
+(function() {
+
+  var dependencies = [
+    "jquery", "underscore",
+    "backbone", "app/controllers/appController",
+    "app/routers/accountRouter"
+  ];
+
+  function app($, _, Backbone, AppController, AccountRouter) {
+
+    //var appController = new AppController(new AccountRouter());
+    //appController.start("55e48de10ea7f36020d38989");
+
+    //Backbone.history.start({pushState: true});
+
+    /*
+    $.ajax({
+    	type:"POST",
+    	url: "/api/login",
+    	contentType: "application/json",
+    	data: JSON.stringify({
+    		emailAddress: "eric@training4developers.com",
+    		password: "test"
+      })
+    }).then(function(result) {
+    	console.dir(result);
+    });
+    */
+
+    /*
+    $.ajax({
+    	type:"GET",
+    	url: "/api/logout"
+    }).then(function(result) {
+    	console.dir(result);
+    });
+    */
+  }
+
+  define(dependencies, app);
+
+})();
